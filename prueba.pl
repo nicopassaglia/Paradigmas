bade05cf6990f87c1911de07eebb05c3540831b6
fawolfmann@gmail.com <fawolfmann@gmail.com>
2018-07-17 14:22:35 -0300

    finalizo domain_def y avance en trans_def

--- prueba.pl
+++ prueba.pl
@@ -1,6 +1,4 @@
 #!/usr/bin/perl
-
-
 use RecDescent;
 
 # $grammar = q(
@@ -9,39 +7,41 @@ use RecDescent;
 # );
 #
 # $parser = new Parse::RecDescent($grammar);
-
+#tidentifier guard_inter(s) transition_def(s? /,/) 
 
 # Create and compile the source file
 
 $parser = Parse::RecDescent->new(q(
   startrule :declarations
 
-
-
   declarations: declaration(s)
   declaration:"define" ("domain" domain_def|"trans" transition_def|"arc" arc_def|"place" place_def|"init" init_def) ';'
-  domain_def: didentifier "=" (denum|dprod|dsetop)
+  domain_def:didentifier "=" (denum|dprod|dsetop)
   didentifier:letter(letter(s?)|digit(s?))
   letter:/\w/
   digit:/\d/
-  transition_def: 't'
+  transition_def: (tidentifier guard_inter(s?))(s /,/) 
+  guard_inter:"guard" guard_def
+  guard_def:'a'
+  tidentifier: didentifier
   arc_def: 'a'
   place_def:'p'
   init_def:'i'
   denum:"{"dvalue(s /,/)"}"
-  dprod:'2'
-  dsetop:'3'
+  dprod: didentifier "X" didentifier
+  dsetop: didentifier setop didentifier
+  setop: "U" | "∩" | "-"
   evaluations:evaluation(s)
   evaluation: /\d+/
   dvalue:string|integer
   string:cap_letter(s)
   integer:digit(s)
   cap_letter:/[A-Z]/
-));
 
+));
 
 # Test it on sample data
 $prueba =<>;
 #print "Valido" if $prueba =~ /c*|a*/;
 #print $parser->startrule;
-defined $parser->startrule($prueba) or die "didn't match";
+defined $parser->startrule($prueba) or die "didn't match \n";
